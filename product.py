04f1f1b4914ff933a942013740d9963d0e736dc1
Jason Lo <lochenan3@gmail.com>
2023-03-19 21:25:19 +0800

    v5_add_check_file_exist

--- product.py
+++ product.py
@@ -1,15 +1,23 @@
-# 讀取檔案
-product = []
+import os # operating system
 
-with open('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue
-        name,price = line.strip().split(',')
-        product.append([name, price])
+product = [] 
+if os.path.isfile('products.csv'):
+    print('yes!')
+    with open('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name,price = line.strip().split(',')
+            product.append([name, price])
+else:
+    print('找不到檔案...')
+
+# 讀取檔案
+
 
 print(product)
 
+# 讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
@@ -22,9 +30,11 @@ while True:
     product.append([name, price])
 print(product)
 
+# 印出所有購買紀錄
 for p in product:
     print(p[0], '的價格是', p[1])
 
+# 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
     f.write('商品,價格\n')
     for p in product:
